376db61471777ec2bc781bdb3bf19a531cb1fd2c
Nurbol <nurboldjusupov@mail.ru>
2023-05-31 20:14:40 +0600

    optimized productInMemoryRepository class

--- SupermarketManagement/Plugins.DataStore.InMemory/ProductInMemoryRepository.cs
+++ SupermarketManagement/Plugins.DataStore.InMemory/ProductInMemoryRepository.cs
@@ -8,35 +8,29 @@ namespace Plugins.DataStore.InMemory
 {
     public class ProductInMemoryRepository : IProductRepository
     {
-        private List<Product> products;
+        private HashSet<Product> products;
+        private int lastProductId;
 
         public ProductInMemoryRepository()
         {
-            // Init with default values
-            products = new List<Product>()
+            lastProductId = 0;
+            products = new HashSet<Product>(capacity: 4)
             {
-                new Product { ProductId = 1, CategoryId = 1, Name = "Iced Tea", Quantity = 100, Price = 1.99},
-                new Product { ProductId = 2, CategoryId = 1, Name = "Canada Dry", Quantity = 200, Price = 1.99},
-                new Product { ProductId = 3, CategoryId = 2, Name = "Whole Wheat Bread", Quantity = 300, Price = 1.50},
-                new Product { ProductId = 4, CategoryId = 2, Name = "White Bread", Quantity = 300, Price = 1.50},
+                new Product { ProductId = GenerateNextProductId(), CategoryId = 1, Name = "Iced Tea", Quantity = 100, Price = 1.99 },
+                new Product { ProductId = GenerateNextProductId(), CategoryId = 1, Name = "Canada Dry", Quantity = 200, Price = 1.99 },
+                new Product { ProductId = GenerateNextProductId(), CategoryId = 2, Name = "Whole Wheat Bread", Quantity = 300, Price = 1.50 },
+                new Product { ProductId = GenerateNextProductId(), CategoryId = 2, Name = "White Bread", Quantity = 300, Price = 1.50 },
             };
         }
 
         public void AddProduct(Product product)
         {
-            if (products.Any(x => x.Name.Equals(product.Name, StringComparison.OrdinalIgnoreCase))) return;
-
-            if (products != null && products.Count > 0)
+            if (products.Any(x => x.Name.Equals(product.Name, StringComparison.OrdinalIgnoreCase)))
             {
-                var maxId = products.Max(x => x.ProductId);
-                product.ProductId = maxId + 1;
-            }
-            else
-            {
-                product.ProductId = 1;
+                return;
             }
 
-
+            product.ProductId = GenerateNextProductId();
             products.Add(product);
         }
 
@@ -65,12 +59,22 @@ namespace Plugins.DataStore.InMemory
         public void DeleteProduct(int productId)
         {
             var productToDelete = GetProductById(productId);
-            if (productToDelete != null) products.Remove(productToDelete);
+            if (productToDelete != null)
+            {
+                products.Remove(productToDelete);
+            }
         }
 
         public IEnumerable<Product> GetProductsByCategoryId(int categoryId)
         {
             return products.Where(x => x.CategoryId == categoryId);
         }
+
+        private int GenerateNextProductId()
+        {
+            lastProductId++;
+            return lastProductId;
+        }
     }
 }
+
